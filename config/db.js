6fb61f7825b1603b44013566ad40e3fe4f1398de
thando mini <tzone85@gmail.com>
2019-08-08 23:34:23 +0200

    installed sqlize and configured db connection

---
+++ config/db.js
@@ -0,0 +1,5 @@
+import {Sequelize} from "sequelize";
+
+const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}/`);
+
+module.exports = sequelize;
