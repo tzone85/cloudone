902c2dd4bf200b4bcb119cb8d2c8b765d50946b9
thando mini <tzone85@gmail.com>
2019-08-09 05:00:55 +0200

    added the crud functionality and returning it in the response

--- services/printer/controllers/PrinterController.js
+++ services/printer/controllers/PrinterController.js
@@ -3,6 +3,46 @@ const printer = require('../../../models/printer')
 
 exports.getPrinters = async (request, reply) => {
   // console.log
-  const models = printer.findAll({ attributes: [`id`, `printer_name`, `printer_ip`, `status`] })
+  printer.findAll({ attributes: [`id`, `printer_name`, `printer_ip`, `status`] })
     .then(response => reply.send(response))
+}
+// const Printer = require('')
+
+exports.createPrinters = async (request, reply) => {
+  const model = printer.save({
+        printer_name: request.body.printer_name,
+        printer_ip: request.body.printer_ip,
+        status: request.body.status,
+        created_at: new Date(),
+        updated_at: new Date()
+      }).then( response => {
+      reply.send(response)
+    }).catch(error => reply.send(error.message))
+}
+
+exports.getPrinter = async (request, reply) => {
+  console.log('Request', request.params.id)
+  printer.findByPk(request.params.id, { attributes: [`id`, `printer_name`, `printer_ip`, `status`] })
+    .then(response => reply.send(response))
+}
+
+exports.updatePrinter = async (request, reply) => {
+  printer.findByPk(request.params.id, { attributes: [`id`, `printer_name`, `printer_ip`, `status`] })
+    .then(response => {
+      response.save({
+        printer_name: request.body.printer_name,
+        printer_ip: request.body.printer_ip,
+        status: request.body.status,
+        updated_at: new Date()
+      })
+      reply.send('')
+    }).catch(error => reply.send(error.message))
+}
+
+exports.deletePrinter = async (request, reply) => {
+  printer.findByPk(request.params.id, { attributes: [`id`] })
+    .then(response => {
+      response.destroy()
+      reply.send('')
+    }).catch(error => reply.send(error.message))
 }
--- services/printer/index.js
+++ services/printer/index.js
@@ -3,9 +3,14 @@ const PrinterController = require('./controllers/PrinterController.js');
 
 module.exports = function (fastify, opts, next) {
     fastify.get('/printers', PrinterController.getPrinters)
+  fastify.post('/printers', PrinterController.createPrinters)
+  fastify.get('/printers/:id', PrinterController.getPrinter)
+  fastify.put('/printers/:id', PrinterController.updatePrinter)
+  fastify.delete('/printers/:id', PrinterController.deletePrinter)
 
     next()
 };
+
 
 // If you prefer async/await, use the following
 //
